aebd0282d34feb27224977bdfbcd840d68c4321f
ZAHIDETEDIK <tamureylem@gmail.com>
2022-04-10 23:56:22 +0200

    nn

--- BACK/app.js
+++ BACK/app.js
@@ -2,32 +2,53 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const helmet = require('helmet');
-
-// Importation des routeurs //
-const userRoutes = require("./routes/user"); // Importation de la route user //
-const messageRoutes = require("./routes/message"); // Importation de la route message //
-const answerRoutes = require("./routes/answer"); // Importation de la route answer //
+const cors=require('cors');
 
 
+//const userRoutes = require('./routes/user');
+//const articleRoutes = require('./routes/article');
+//const commentRoutes = require('./routes/comment');
+//const likeRoutes = require('./routes/likes');
 
 
 const app = express();
+require('dotenv').config();
+const mysql = require("mysql");
+const { connect } = require('http2');
+const db = mysql.createConnection({ 
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME
+});
+db.connect(error => {
+    if (error) throw error;
+    console.log('Connexion à la base de données réussie !')
+});
+module.exports=connect;
 
+
+app.use(helmet());
+app.use (cors);
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // On accéde à l'API depuis diverses origines //
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); // Liste requêtes autorisées //
+    res.setHeader(
+        'Access-Control-Allow-Origin', '*'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+    );
     next();
 });
+//app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(bodyParser.json());
 
-app.use(bodyParser.json()); // App requiert BodyParser //
-app.use(helmet());
-app.use('/images', express.static(path.join(__dirname, 'images'))); // Pour toute requête envoyée à /images/, on sert ce dossier statique image //
+//app.use('/api/auth', userRoutes);
+//app.use('/api/auth', articleRoutes);
+//app.use('/api/auth', commentRoutes);
+//app.use('/api/auth', likeRoutes);
 
-// Enregistrement des routeurs //
-app.use('/api/auth', userRoutes);
-app.use('/api/messages', messageRoutes); 
-app.use('/api/answers', answerRoutes); 
-
-
-module.exports = app; // On exporte app /
+module.exports = app;
